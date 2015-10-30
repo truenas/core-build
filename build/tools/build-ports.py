3af2a6aa9f0cf652a00447147968797067dcc9f1
Jakub Klama <jakub@ixsystems.com>
2015-10-30 13:19:44 +0100

    Add SUDO_USER name to jail name if available, to avoid jail name conflicts with multiple build running in parallel

--- build/tools/build-ports.py
+++ build/tools/build-ports.py
@@ -105,6 +105,10 @@ def create_ports_list():
 def obtain_jail_name():
     global jailname
     for i in string.ascii_lowercase:
+        user = env('${SUDO_USER}')
+        if user:
+            i = e('${i}-${user}')
+
         if sh('jls -q -n -j j${i}-p', log="/dev/null", nofail=True) != 0:
             jailname = e('j${i}')
             setfile(e('${OBJDIR}/jailname'), jailname)
